a606e997bbfff844d983809b24d2bf98db738e00
Purekatana <purekatana@gmail.com>
2023-04-14 18:48:02 -0400

    Added a system that sync the server and client time for match countdown time

--- Source/BlastWars/PlayerController/BlasterPlayerController.h
+++ Source/BlastWars/PlayerController/BlasterPlayerController.h
@@ -25,14 +25,35 @@ public:
 	void SetHUDWeaponType(FText WeaponType);
 	void SetHUDMatchCountdown(float CountdownTime);
 	void HideEliminatedText();
-
 	virtual void OnPossess(APawn* InPawn) override;
+	// Synced with server world clock
+	virtual float GetServerTime();
+	// Sync with server clock as soon as possible
+	virtual void ReceivedPlayer() override;
 
 protected:
 
 	virtual void BeginPlay() override;
 	void SetHUDTime();
 	virtual void Tick(float DeltaTime) override;
+
+	// Sync time between clients and server
+
+	// Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+	// Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+	// Difference between Client and Server time
+	float ClientServerDelta = 0.f;
+	UPROPERTY(EditAnywhere, Category = Time)
+	// Makes sure we send another request every frequency to make sure we get the correct server time;
+	float TimeSyncFrequency = 5.f;
+	float TimeSyncRunningTime = 0.f;
+	void CheckTimeSync(float DeltaTime);
+
+
 private:
 
 	UPROPERTY()
